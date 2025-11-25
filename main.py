88f7e5d4c6553b6ac9a616a5b3e213a336807a8e
Nyges <Demur.Erdogan.27@gmail.com>
2025-11-25 19:57:03 +0300

    Update main.py
    just a start

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import json
+accounts={"admin":{"password":"secretadminpassword"}}
 
+while True:
+    choice=int(input("Please Login or Create an account\n1. Create Account\n2. Login\n"))
+    if choice==1:
+        newUsername=input("Enter Username: ")
+        newPassword=input("Enter Password: ")
+        accounts.update({newUsername:{"password":newPassword,"balance":0}})
+        print(f"Account '{newUsername}' successfully created, please log in again")
+    elif choice==2:
+        print(":)") #todo
+    else:
+        print("No such option")
